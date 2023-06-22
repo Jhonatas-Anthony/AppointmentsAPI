eb8420aeb0d08beb8d868ce6efe40bddf600f027
Jhonatas-Anthony <jhonatasanthony6@gmail.com>
2023-06-21 21:02:14 -0300

    correções do projeto

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
 
 // Conectando ao banco de dados
 connectDB()
 
-// Aplica o middleware globalmente antes de todas as rotas
-
 // Rotas
 app.use('/user', user)
 app.use('/time', timeTable)
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'modules', 'dashboard/index.html'));
@@ -34,8 +34,12 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'modules', 'user/login.html'));
 });
 
-app.use('/timetable', (req, res) => {
+app.use('/timetable', authMiddleware, (req, res) => {
     res.sendFile(path.join(__dirname, 'modules', 'agenda/viewTime.html'))
+})
+
+app.use('/fulltimetable', authMiddleware, (req, res) => {
+    res.sendFile(path.join(__dirname, 'modules', 'agenda/viewOtherTime.html'))
 })
 
 app.use('/schedule', authMiddleware, (req, res) => {
