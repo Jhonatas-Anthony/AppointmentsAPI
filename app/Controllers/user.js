a71bfbe438e2f17493b4ed8b7d6f22a7b3838859
Jhonatas-Anthony <jhonatasanthony6@gmail.com>
2023-06-23 02:03:12 -0300

    correção de erros e limpeza dos controllers

--- app/Controllers/user.js
+++ app/Controllers/user.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const User = require('../Models/user');
 const jwt = require('jsonwebtoken');
-//const cookieParser = require('cookie-parser');
 
 const userRouter = express.Router();
 const signToken = id => {
@@ -15,33 +14,29 @@ userRouter.post('/signup', async (req, res) => {
     try {
         const { name, email, password, confirmPassword } = req.body;
 
-        // Verificar se a senha e a confirmação de senha coincidem
         if (password !== confirmPassword) {
             return res.status(400).json({ message: 'As senhas não coincidem' });
         }
 
-        // Verificar se o usuário já existe com o mesmo email
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ message: 'Já existe um usuário com esse email' });
         }
 
         // Criar um hash da senha antes de salvar no banco de dados
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash(password, parseInt(process.env.BCRYPT_SEED));
 
-        // Criar o novo usuário
         const newUser = new User({
             name,
             email,
             password: hashedPassword,
         });
 
-        // Salvar o usuário no banco de dados
         await newUser.save();
 
         res.status(201).redirect('/login');
     } catch (error) {
-        console.error(error);
+        //console.error(error);
         res.status(500).json({ message: 'Erro ao cadastrar o usuário' });
     }
 });
@@ -49,14 +44,12 @@ userRouter.post('/signup', async (req, res) => {
 userRouter.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body;
-        console.log(User)
-        // Verificar se o usuário existe com o mesmo email
+
         const user = await User.findOne({ email });
         if (!user) {
             return res.status(400).json({ message: 'Email inválido' });
         }
 
-        // Verificar se a senha é válida
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
             return res.status(400).json({ message: 'Senha incorreta' });
@@ -67,7 +60,7 @@ userRouter.post('/login', async (req, res) => {
         res.status(200).redirect('/');
 
     } catch (error) {
-        console.error(error);
+        //console.error(error);
         res.status(500).json({ message: 'Erro ao realizar o login' });
     }
 });
@@ -77,21 +70,19 @@ userRouter.get('/logout', async (req, res) => {
     res.redirect('/login')
 })
 
+//Essa rota funcionará como um middleware
 userRouter.get('/checkAuth', async (req, res) => {
+    // Se esta área estiver retornando undefined, o usuário não está autenticado
     if(req.headers.cookie == undefined){
         return res.json({ isAuthenticated: false });
     }
     try {
-
-
         const token = req.headers.cookie.split('token=')[1]
-
-        console.log(token)
         if (!token) {
             // Se o token não estiver presente, o usuário não está autenticado
             return res.json({ isAuthenticated: false });
         }
-
+        //Outro try, para verificar se o token presente, é válido.
         try {
             // Verifica se o token é válido
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -104,29 +95,30 @@ userRouter.get('/checkAuth', async (req, res) => {
             }
 
             // Se o usuário existir
-            return res.json({ isAuthenticated: true });
+            return res.json({ isAuthenticated: true, user: user });
         } catch (error) {
             return res.json({ isAuthenticated: false });
         }
     } catch (error) {
-        console.error(error);
+        //console.error(error);
         res.status(500).json({ message: 'Erro ao verificar a autenticação do usuário' });
     }
 });
 
+//Rota para popular o banco com usuários teste
 userRouter.get('/populate', async (req, res) => {
     const data = [
         { name: 'Jhonatas Anthony', email: 'jhonatas@gmail.com', password: await bcrypt.hash('123', 10) },
-        { name: 'Garrincha', email: 'adm@gmail.com', password: await bcrypt.hash('123', 10) },
-        { name: 'Luca', email: 'sub@gmail.com', password: await bcrypt.hash('123', 10) },
-        { name: 'Caruso', email: 'grosso@gmail.com', password: await bcrypt.hash('123', 10) },
-        { name: 'Lilica', email: 'lex@gmail.com', password: await bcrypt.hash('123', 10) }
+        { name: 'Garrincha Medeiros', email: 'adm@gmail.com', password: await bcrypt.hash('123', 10) },
+        { name: 'Lucca Tattine', email: 'sub@gmail.com', password: await bcrypt.hash('123', 10) },
+        { name: 'Caruso Ignácio', email: 'grosso@gmail.com', password: await bcrypt.hash('123', 10) },
+        { name: 'Lilian Carol', email: 'lex@gmail.com', password: await bcrypt.hash('123', 10) }
     ]
     try {
         const users = await User.insertMany(data)
         res.status(200).json({ message: 'banco populado com sucesso', users });
     } catch (error) {
-        console.error(error);
+        //console.error(error);
         res.status(500).json({ message: 'Erro ao popular banco' })
     }
 })
